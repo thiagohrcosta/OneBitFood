c4a546ced9d1d9f6ca69bf49dcabc06dad0edc0f
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-10 16:21:58 -0300

    Order Controller - create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,11 @@
 class OrdersController < ApplicationController
   def create
+    order = Order.new(order_params)
+    if order.save
+      render json: @order, status: :created
+    else
+      render json: order.errors, stratus: :unprocessable_entity
+    end
   end
 
   def show
