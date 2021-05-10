e6fcd183adde2f0cd08f7c21afc67556a9634e59
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-10 16:04:28 -0300

    Adds filter with ransack gem

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -13,4 +13,8 @@ class RestaurantsController < ApplicationController
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def filter_by_query
+    @restaurant = @restaurants.ransack(name_or_description_cont: params[:q]).result
+  end
 end
