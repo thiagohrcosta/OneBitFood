99f6636c62edc627bb3c42c8373d4a9c80a83536
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-14 16:37:34 -0300

    Finishes UseEffect from home page

--- onebitfood-client/pages/index.js
+++ onebitfood-client/pages/index.js
@@ -1,13 +1,14 @@
 import { Button, Row, Col } from 'react-bootstrap';
 import Link from 'next/link';
 import { FaCrosshairs } from 'react-icons/fa';
+import Typewriter from '../components/Typewriter';
 
 export default function Home() {
   return (
     <Row className="mt-8 justify-content-center">
       <Col md={7} xs={12} className="text-center">
         <h1 className='fw-bolder text-custom-gray-darker mb-5 1h-base display-5 '>
-          Comida saudável e gostosa direto na sua casa
+          <Typewriter text='Comida saudável e gostosa direto na sua casa'/>
         </h1>
         <Link href='/restaurants'>
           <Button variant="custom-red" size="lg" className='text-white'>
