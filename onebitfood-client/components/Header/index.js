bb7737821d01f82dd8b24c65c62955a6d33fdc1f
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-14 21:10:57 -0300

    Adds search bar

--- onebitfood-client/components/Header/index.js
+++ onebitfood-client/components/Header/index.js
@@ -1,23 +1,29 @@
-import { Navbar } from 'react-bootstrap';
-import Image from 'next/image';
-import Link from 'next/link';
+import { Navbar } from "react-bootstrap";
+import Image from "next/image";
+import Link from "next/link";
+import SearchBox from '../SearchBox'
 
 export default function Header() {
-    return (
-        <Navbar bg='white' expand='lg' className='border-bottom border-custom-gray'>
-            <Navbar.Brand className='mx-3'>
-                <Link href='/restaurants'>
-                    <a>
-                        <Image 
-                            src='/logo.png'
-                            alt="OneBitFood"
-                            width={200}
-                            height={44}    
-                            className='clickable_effect'
-                        />
-                    </a>
-                </Link>
-            </Navbar.Brand>            
-        </Navbar>
-    )
+  return (
+    <Navbar bg="white" expand="lg" className="border-bottom border-custom-gray">
+      <Navbar.Brand className='mx-3' >
+        <Link href="/restaurants">
+          <a>
+            <Image
+              src="/logo.png"
+              alt="OneBitFood"
+              width={200}
+              height={44}
+              className="clickable_effect"
+            />
+          </a>
+        </Link>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
+      <Navbar.Collapse id='responsive-navbar-nav' className='justify-content-end'>
+        <SearchBox />
+      </Navbar.Collapse>
+      
+    </Navbar>
+  );
 }
