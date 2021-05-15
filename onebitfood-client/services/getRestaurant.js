362ae35089118ecf6a011fc07a2920110d248bae
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-14 21:33:17 -0300

    Finishes restaurant details

--- onebitfood-client/services/getRestaurant.js
+++ onebitfood-client/services/getRestaurant.js
@@ -3,11 +3,10 @@ import useSWR from "swr";
 export default function getRestaurant(id) {
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
-  const { data, error } = useSWR(
-    `${process.env.apiUrl}/api/restaurants/${id}`,
-    fetcher,
-    { revalidateOnFocus: false }
+  const { data, error } = useSWR( id ? `${process.env.apiUrl}/api/restaurants/${id}` : null,
+    fetcher, { revalidateOnFocus: false }
   );
+  
   return {
     restaurant: data,
     isLoading: !error && !data,
