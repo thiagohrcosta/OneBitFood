81063a18af76ec790c852eba03eb2374d2bb1ce6
Thiago Costa <thiagohrcosta86@gmail.com>
2021-05-15 21:22:51 -0300

    Finishes address modal

--- onebitfood-client/services/getRestaurants.js
+++ onebitfood-client/services/getRestaurants.js
@@ -1,15 +1,21 @@
 import useSWR from "swr";
 import { useRouter } from 'next/router';
+import { useRecoilState } from 'recoil';
+import AddressState from '../store/atoms/addressAtom';
+import addressState from "../store/atoms/addressAtom";
 
 export default function getRestaurants() {
   const router = useRouter();
   const { category, q } = router.query;
+  const [address] = useRecoilState(addressState)
 
   let params = '';
   if(category)
     params= `${params == '' ? '?' : '&'}category=${category}`
   if(q)
     params= `${params == '' ? '?' : '&'}q=${q}`
+  if(address.city != '')
+    params = `${params == '' ? '?' : '&'}city=${address.city}`
 
   const fetcher = (...args) => fetch(...args).then((res) => res.json());
   const { data, error } = useSWR(
